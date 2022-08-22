7cb383a7a5401deb0ad0ada680e01ed592533d85
Ditokasiditz <111700359+Ditokasiditz@users.noreply.github.com>
2022-08-22 11:36:07 +0700

    Add files via upload

---
+++ week3_meth2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+	int n, i, j, f;
+	printf("type number:");
+	scanf("%d", &n);
+		
+	if (n >= 1){
+		i = 0;
+		while(i < n){
+			i++;
+				j = 0;
+				while(j <= n){		
+					if (((j > 1)&&(j < n))&&((i > 1)&&(i < n))) {
+						printf(" "); 
+					}
+					else if((j !=0 )&&(i!=0)){
+						printf("*");
+					}
+
+					j++;
+				}
+			printf("\n");			
+
+		}
+	}
+return 0;
+}
